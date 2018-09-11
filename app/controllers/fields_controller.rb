16d131263825e418312917096699c89674774322
JUNIOR <junior@GAVILA1-PC.local>
2018-09-10 20:18:42 -0500

    login to field

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,5 +1,6 @@
 class FieldsController < ApplicationController
   before_action :set_field, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /fields
   # GET /fields.json
